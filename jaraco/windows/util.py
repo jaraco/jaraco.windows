37e89a24c89a3db5b07b09c8b76191f527be81c9
jaraco <none@none>
2009-02-01 01:50:23 +0000

    Adding timezone support

--- jaraco/windows/util.py
+++ jaraco/windows/util.py
@@ -10,3 +10,12 @@ def ensure_unicode(param):
 	except TypeError:
 		pass # just return the param as is
 	return param
+
+class Extended(object):
+	"Used to add extended capability to structures"
+	def __eq__(self, other):
+		return buffer(self) == buffer(other)
+		
+	def __ne__(self, other):
+		return buffer(self) != buffer(other)
+
