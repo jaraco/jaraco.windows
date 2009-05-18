78c4f2471c69db2261e9aa6c229a3998f5843ff3
jaraco <none@none>
2009-05-18 03:19:08 +0000

    Added true symlink detection

--- jaraco/windows/filesystem.py
+++ jaraco/windows/filesystem.py
@@ -2,8 +2,11 @@
 
 import os
 import sys
-from ctypes import Structure, windll
-from ctypes.wintypes import BOOLEAN, LPWSTR, DWORD, LPVOID
+from ctypes import Structure, windll, POINTER, byref, cast
+from ctypes.wintypes import (
+	BOOLEAN, LPWSTR, DWORD, LPVOID, HANDLE, FILETIME,
+	c_uint64, WCHAR,
+	)
 from jaraco.windows.error import handle_nonzero_success, WindowsError
 
 CreateSymbolicLink = windll.kernel32.CreateSymbolicLinkW
@@ -25,6 +28,33 @@ CreateHardLink.restype = BOOLEAN
 GetFileAttributes = windll.kernel32.GetFileAttributesW
 GetFileAttributes.argtypes = (LPWSTR,)
 GetFileAttributes.restype = DWORD
+
+MAX_PATH = 260
+
+class WIN32_FIND_DATA(Structure):
+	_fields_ = [
+		('file_attributes', DWORD),
+		('creation_time', FILETIME),
+		('last_access_time', FILETIME),
+		('last_write_time', FILETIME),
+		('file_size_words', DWORD*2),
+		('reserved', DWORD*2),
+		('filename', WCHAR*MAX_PATH),
+		('alternate_filename', WCHAR*14),
+	]
+	
+	@property
+	def file_size(self):
+		return cast(self.file_size_words, POINTER(c_uint64)).contents
+
+LPWIN32_FIND_DATA = POINTER(WIN32_FIND_DATA)
+
+FindFirstFile = windll.kernel32.FindFirstFileW
+FindFirstFile.argtypes = (LPWSTR, LPWIN32_FIND_DATA)
+FindFirstFile.restype = HANDLE
+FindNextFile = windll.kernel32.FindNextFileW
+FindNextFile.argtypes = (HANDLE, LPWIN32_FIND_DATA)
+FindNextFile.restype = BOOLEAN
 
 def mklink():
 	"""
@@ -57,17 +87,59 @@ def link(link, target):
 	"""
 	handle_nonzero_success(CreateHardLink(link, target, None))
 
-def islink(path):
+def is_reparse_point(path):
 	"""
-	Determine if the given path is a symlink.
-	
-	@TODO: this code currently only determines if the target is a reparse
-	point.  See http://stackoverflow.com/questions/221417/how-do-i-programmatically-access-the-target-path-of-a-windows-symbolic-link
-	for more info on how to more accurately determine if it's a symlink.
+	Determine if the given path is a reparse point.
 	"""
 	FILE_ATTRIBUTE_REPARSE_POINT = 0x400
 	INVALID_FILE_ATTRIBUTES = 0xFFFFFFFF
 	res = GetFileAttributes(path)
-	print res
 	if res == INVALID_FILE_ATTRIBUTES: raise WindowsError()
 	return bool(res & FILE_ATTRIBUTE_REPARSE_POINT)
+
+def islink(path):
+	"Determine if the given path is a symlink"
+	return is_reparse_point(path) and is_symlink(path)
+
+def is_symlink(path):
+	"""
+	Assuming path is a reparse point, determine if it's a symlink.
+	"""
+	return _is_symlink(next(find_files(path)))
+
+def _is_symlink(find_data):
+	IO_REPARSE_TAG_SYMLINK = 0xA000000C
+	return find_data.reserved[0] == IO_REPARSE_TAG_SYMLINK
+
+def find_files(spec):
+	"""
+	A pythonic wrapper around the FindFirstFile/FindNextFile win32 api.
+	
+	>>> root_files = tuple(find_files(r'c:\*'))
+	>>> len(root_files) > 1
+	True
+	>>> root_files[0].filename == root_files[1].filename
+	False
+	
+	This test might fail on a non-standard installation
+	>>> 'Windows' in (fd.filename for fd in root_files)
+	True
+	"""
+	fd = WIN32_FIND_DATA()
+	INVALID_FILE_HANDLE = 0xFFFFFFFF
+	ERROR_NO_MORE_FILES = 0x12
+	handle = FindFirstFile(spec, byref(fd))
+	while True:
+		if handle == INVALID_FILE_HANDLE:
+			raise WindowsError()
+		yield fd
+		fd = WIN32_FIND_DATA()
+		res = FindNextFile(handle, byref(fd))
+		if res == 0: # error
+			error = WindowsError()
+			if error.value == ERROR_NO_MORE_FILES:
+				break
+			else: raise error
+	# todo: how to close handle when generator is destroyed?
+	# hint: catch GeneratorExit
+	windll.kernel32.FindClose(handle)
