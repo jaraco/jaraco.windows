8466d3bf114bcdd54ebc1ebd0001638a610dc52b
jaraco <none@none>
2010-05-03 14:31:10 +0000

    Updated converter with documentation and slight refactoring to enable custom target formats

--- jaraco/windows/office.py
+++ jaraco/windows/office.py
@@ -1,18 +1,30 @@
 import os
 from win32com.client import Dispatch, constants
 
-from jaraco.util.filesystem import save_to_file
+from jaraco.util.filesystem import save_to_file, replace_extension
 
 class Converter(object):
+	"""
+	An object that will convert a Word-readable file to one of the Word-
+	savable formats (defaults to PDF).
+	
+	Requires Microsoft Word 2007 or later.
+	"""
+	target_format = getattr(constants, 'wdFormatPDF', 17)
+
 	def __init__(self):
 		self.word = Dispatch('Word.Application')
 
 	def convert(self, docfile_string):
+		"""
+		Take a string (in memory) and return it as a string of the
+		target format (also as a string in memory).
+		"""
 		with save_to_file(docfile_string) as docfile:
 			doc = self.word.Documents.Open(docfile)
-			wdFormatPDF = getattr(constants, 'wdFormatPDF', 17)
-			pdffile = docfile+'.pdf' # if I don't put a pdf extension on it, Word will
-			res = doc.SaveAs(pdffile, wdFormatPDF)
+			# if I don't put a pdf extension on it, Word will
+			pdffile = replace_extension('.pdf', docfile)
+			res = doc.SaveAs(pdffile, self.target_format)
 			wdDoNotSaveChanges = getattr(constants, 'wdDoNotSaveChanges', 0)
 			doc.Close(wdDoNotSaveChanges)
 			content = open(pdffile, 'rb').read()
@@ -23,4 +35,3 @@ class Converter(object):
 
 	def __del__(self):
 		self.word.Quit()
-
