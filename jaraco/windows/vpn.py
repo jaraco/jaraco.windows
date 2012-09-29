b3293f960f7bbb568374f5c9c0aba39c1474a5d7
Jason R. Coombs <jaraco@jaraco.com>
2012-09-28 20:02:25 -0400

    Can't use dictionary

--- jaraco/windows/vpn.py
+++ jaraco/windows/vpn.py
@@ -1,11 +1,7 @@
 import os
 from path import path
 
-def params_to_lines(params):
-	return ('{key}={value}\n'.format(**vars())
-		for key, value in params.iteritems())
-
-def install_pptp(name, **params):
+def install_pptp(name, param_lines):
 	"""
 	"""
 	# or consider using the API: http://msdn.microsoft.com/en-us/library/aa446739%28v=VS.85%29.aspx
@@ -14,5 +10,5 @@ def install_pptp(name, **params):
 	pbk_path.dirname().makedirs_p()
 	with open(pbk_path, 'a') as pbk:
 		pbk.write('[{name}]\n'.format(name=name))
-		pbk.writelines(params_to_lines(params))
+		pbk.writelines(param_lines)
 		pbk.write('\n')
