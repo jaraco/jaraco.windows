3ea85ecd58d53b5c6dbdc617b33ad65f69060e33
Jason R. Coombs <jaraco@jaraco.com>
2012-11-20 22:56:05 -0500

    Merge two drafts into correct module

--- jaraco/windows/security.py
+++ jaraco/windows/security.py
@@ -1,5 +1,6 @@
 import ctypes.wintypes
 
+from jaraco.windows.error import handle_nonzero_success
 
 class TokenInformationClass:
 	TokenUser = 1
@@ -17,7 +18,24 @@ def GetTokenInformation(token, information_class):
 	"""
 	data_size = ctypes.wintypes.DWORD()
 	info = information_class()
-	ctypes.windll.advapi32.GetTokenInformation(token, information_class.num,
-		ctypes.by_ref(info), ctypes.sizeof(information_class),
-		ctypes.by_ref(data_size))
+	handle_nonzero_success(ctypes.windll.advapi32.GetTokenInformation(token,
+		information_class.num,
+		ctypes.byref(info), ctypes.sizeof(information_class),
+		ctypes.byref(data_size)))
 	return info
+
+class TokenAccess:
+	TOKEN_QUERY = 0x8
+
+def OpenProcessToken(proc_handle, access):
+	result = ctypes.wintypes.HANDLE()
+	ctypes.windll.advapi32.OpenProcessToken(
+		proc_handle, access, ctypes.byref(result))
+	return result
+
+def get_current_user():
+	process = OpenProcessToken(
+		ctypes.windll.kernel32.GetCurrentProcess(),
+		TokenAccess.TOKEN_QUERY,
+	)
+	return GetTokenInformation(process, TOKEN_USER)
