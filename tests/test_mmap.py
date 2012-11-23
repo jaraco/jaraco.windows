3d215043a03193b00c7d9f486570bf19460c9ac7
Jason R. Coombs <jaraco@jaraco.com>
2012-11-23 13:48:00 -0500

    Added test to capture need to read null bytes.

--- tests/test_mmap.py
+++ tests/test_mmap.py
@@ -8,3 +8,10 @@ def test_mmap_simple():
 		map.seek(0)
 		assert map.read(4) == 'abcd'
 		assert map.read(2) == 'ef'
+
+def test_mmap_null_bytes():
+	map = mmap.MemoryMap('foo', 50)
+	with map:
+		map.write('abcdef')
+		map.seek(0)
+		assert map.read(10) == 'abcdef\x00\x00\x00\x00'
