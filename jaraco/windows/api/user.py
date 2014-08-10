a675f10bb41dbe35a41f27ce4d0cc5579943dfbc
Jason R. Coombs <jaraco@jaraco.com>
2014-08-10 08:38:04 -0400

    LPDWORD is defined in wintypes

--- jaraco/windows/api/user.py
+++ jaraco/windows/api/user.py
@@ -1,6 +1,5 @@
 import ctypes.wintypes
 
-LPDWORD = ctypes.POINTER(ctypes.wintypes.DWORD)
 GetUserName = ctypes.windll.advapi32.GetUserNameW
-GetUserName.argtypes = ctypes.wintypes.LPWSTR, LPDWORD
+GetUserName.argtypes = ctypes.wintypes.LPWSTR, ctypes.wintypes.LPDWORD
 GetUserName.restype = ctypes.wintypes.DWORD
