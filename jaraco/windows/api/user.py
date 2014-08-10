3b8b0b9022bb71d52512c37fed49113fc3cbaaca
Jason R. Coombs <jaraco@jaraco.com>
2014-08-10 08:36:53 -0400

    Remove superfluous parentheses

--- jaraco/windows/api/user.py
+++ jaraco/windows/api/user.py
@@ -2,5 +2,5 @@ import ctypes.wintypes
 
 LPDWORD = ctypes.POINTER(ctypes.wintypes.DWORD)
 GetUserName = ctypes.windll.advapi32.GetUserNameW
-GetUserName.argtypes = (ctypes.wintypes.LPWSTR, LPDWORD)
+GetUserName.argtypes = ctypes.wintypes.LPWSTR, LPDWORD
 GetUserName.restype = ctypes.wintypes.DWORD
