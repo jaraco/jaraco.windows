58904a91dcdae9c44412447d0a21ce7d9c40bef8
Jason R. Coombs <jaraco@jaraco.com>
2014-08-12 16:22:55 -0400

    Use unicode literals in tests

--- tests/test_clipboard.py
+++ tests/test_clipboard.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 import jaraco.windows.clipboard as wc
 
 def test_unicode_clipboard():
 	wc.set_unicode_text('foo'*100)
-	assert wc.get_unicode_text() == u'foo'*100
-	wc.set_unicode_text(u'☃')
-	assert wc.get_unicode_text() == u'☃'
+	assert wc.get_unicode_text() == 'foo'*100
+	wc.set_unicode_text('☃')
+	assert wc.get_unicode_text() == '☃'
