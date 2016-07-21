59a565d12f5f7f8d9f8a166d8c27eaea7b6c48b0
Jason R. Coombs <jaraco@jaraco.com>
2016-07-21 09:47:49 -0400

    Allow tests to be executed on non-Windows systems such that tagged releases can be made on Travis-CI.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,9 @@
+import platform
+import os
+
+
 collect_ignore = []
+
 
 try:
 	__import__('win32api')
@@ -7,4 +12,17 @@ except ImportError:
 	collect_ignore += [
 		'jaraco/windows/eventlog.py',
 		'jaraco/windows/services.py',
+	]
+
+
+if platform.system() != 'Windows':
+	"""
+	Ignore everything but 'test_root' on non-Windows systems
+	"""
+	collect_ignore = [
+		os.path.join(root, filename)
+		for root, dirs, files in os.walk('.')
+		for filename in files
+		if filename.endswith('.py')
+		and not filename.startswith('test_root')
 	]
---
+++ tests/test_root.py
@@ -0,0 +1,5 @@
+def test_namespace():
+	"""
+	A trivially simple test that will run on all platforms.
+	"""
+	__import__('jaraco')
