a4f92286325b151f7b5a9d886d9e42723e35f2dc
Jason R. Coombs <jaraco@jaraco.com>
2016-08-19 10:15:25 -0400

    Normalize whitespace

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,14 +1,16 @@
 from jaraco.windows import filesystem
 
+
 def test_islink_on_nonexistent_target():
 	assert filesystem.islink('/doesnotexist') == False
+
 
 def test_is_symlink(tmpdir):
 	with tmpdir.as_cwd():
 		filesystem.symlink('foobaz', 'foobar')
 		assert filesystem.is_symlink('foobar')
 
+
 def test_FileAttributes():
 	attrs = filesystem.FileAttributes.get(__file__)
 	assert not attrs.hidden
-
