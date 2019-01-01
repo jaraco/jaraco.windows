51811efed50b46ad08daa25408a1cc806bc8d519
Jason R. Coombs <jaraco@jaraco.com>
2019-01-01 12:04:41 -0500

    Switch to pkgutil namespace technique. Ref jaraco/jaraco.functools#9

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,10 @@
+4.0
+===
+
+Switch to `pkgutil namespace technique
+<https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages>`_
+for the ``jaraco`` namespace.
+
 3.9.2
 =====
 
--- jaraco/__init__.py
+++ jaraco/__init__.py
@@ -1,10 +1,1 @@
-# this is a namespace package
-__import__('pkg_resources').declare_namespace(__name__)
-
-try:
-	# py2exe support (http://www.py2exe.org/index.cgi/ExeWithEggs)
-	import modulefinder
-	for p in __path__:
-		modulefinder.AddPackagePath(__name__, p)
-except ImportError:
-	pass
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
