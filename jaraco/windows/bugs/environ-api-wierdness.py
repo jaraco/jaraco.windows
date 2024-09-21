cace1c7f5a169b1f75fa0adb00c99f6195502735
Jason R. Coombs <jaraco@jaraco.com>
2024-09-21 12:13:37 -0400

    Refresh environ-api-wierdness.

--- jaraco/windows/bugs/environ-api-wierdness.py
+++ jaraco/windows/bugs/environ-api-wierdness.py
@@ -1,6 +1,11 @@
+"""
+Script illustrates the surprising behaviors around set/get on the environment
+with various APIs.
+"""
+
 import ctypes
-from jaraco.windows import environ
-import os
+from jaraco.windows import environ  # noqa: F401
+import os  # noqa: F401
 
 getenv = ctypes.cdll.msvcrt.getenv
 getenv.restype = ctypes.c_char_p
@@ -12,26 +17,36 @@ def do_putenv(*pair):
 
 
 def print_environment_variable(key):
-    for method in (os.environ.get, os.getenv, environ.GetEnvironmentVariable, getenv):
+    methods = (
+        'os.environ.get',
+        'os.getenv',
+        'environ.GetEnvironmentVariable',
+        'getenv',
+    )
+    for method in methods:
+        func = eval(method)
+        print(method, end=': ')
         try:
-            print(repr(method(key)))
+            print(repr(func(key)))
         except Exception as e:
-            print(e, end=' ')
-    print
+            print(e)
+    print()
 
 
 def do_test():
     key = 'TEST_PYTHON_ENVIRONMENT'
+    print('Before setting anything', end=':\n')
     print_environment_variable(key)
     methods = (
-        os.environ.__setitem__,
-        os.putenv,
-        environ.SetEnvironmentVariable,
-        do_putenv,
+        'os.environ.__setitem__',
+        'os.putenv',
+        'environ.SetEnvironmentVariable',
+        'do_putenv',
     )
-    for i, method in enumerate(methods):
-        print('round', i)
-        method(key, 'value when using method %d' % i)
+    for method in methods:
+        print('Setting with', method, end=':\n')
+        func = eval(method)
+        func(key, f'value when using method {method}')
         print_environment_variable(key)
 
 
