7e84aaf8f3074db439f9ed0db1e108c31d9aa610
Jason R. Coombs <jaraco@jaraco.com>
2025-05-28 21:30:36 -0400

    Added tempfile module with resilient TemporaryDirectory.

---
+++ jaraco/windows/tempfile.py
@@ -0,0 +1,23 @@
+import tempfile
+
+from tempora.timing import BackoffDelay
+
+from jaraco.functools import retry
+
+
+class TemporaryDirectory(tempfile.TemporaryDirectory):
+    """
+    A temp directory that attempts to be more
+    resilient to PermissionErrors on Windows when deleting files
+    that might be held open.
+
+    It tries to delete files/directories multiple times with a short delay.
+    """
+
+    @retry(
+        retries=3,
+        trap=PermissionError,
+        cleanup=BackoffDelay(delay=0.5, factor=2),
+    )
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
